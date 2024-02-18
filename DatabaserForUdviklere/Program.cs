878cba6b8c8b357cf3ba870238969d9a3d748661
Thomas Dall Pedersen <tempo@mail.dk>
2024-02-18 17:00:51 +0100

    wip

--- DatabaserForUdviklere/Program.cs
+++ DatabaserForUdviklere/Program.cs
@@ -1,10 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DatabaserForUdviklere.Models;
+using System.Reflection;
 
 
 using (var context = new GlobalogDevelopmentContext())
 {
-    var flight = context.Flights.Where(f => f.Id == 10108396);
+    var flight = context.Flights.FirstOrDefault(f => f.Id == 10108396);
+    if (flight != null)
+    {
+        PropertyInfo[] properties = flight.GetType().GetProperties();
 
-    Console.WriteLine(flight.ToString());
+        foreach (PropertyInfo property in properties)
+        {
+            object value = property.GetValue(flight, null) ?? "null";
+            Console.WriteLine($"{property.Name}: {value}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Flight not found.");
+    }
 }
+
+
+
